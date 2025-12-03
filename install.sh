543c5ee39b896205167a6ad25f583db08ebc668a
kofe16 <kofekevin16@gmail.com>
2025-12-03 08:12:53 -0500

    Add .hypr/

--- install.sh
+++ install.sh
@@ -20,19 +20,28 @@ backup_and_link() {
     ln -sf "$src" "$dest"
 }
 
+# Hyprland configs
+mkdir -p "$HOME/.config/hypr"
+for file in "$CONFIG_DIR/hypr/"*; do
+    [ -e "$file" ] || continue
+    backup_and_link "$file" "$HOME/.config/hypr/$(basename $file)"
+done
+
 # Waybar configs
 mkdir -p "$HOME/.config/waybar"
 for file in "$CONFIG_DIR/waybar/"*; do
+    [ -e "$file" ] || continue
     backup_and_link "$file" "$HOME/.config/waybar/$(basename $file)"
 done
 
 # Nwg-displays configs
 mkdir -p "$HOME/.config/nwg-displays"
 for file in "$CONFIG_DIR/nwg-displays/"*; do
+    [ -e "$file" ] || continue
     backup_and_link "$file" "$HOME/.config/nwg-displays/$(basename $file)"
 done
 
-# SDDM configs sudo
+# SDDM configs (system-wide, requiere sudo)
 # Themes
 for file in "$CONFIG_DIR/sddm/themes/"*; do
     if [ -e "/usr/share/sddm/themes/$(basename $file)" ]; then
@@ -43,7 +52,7 @@ for file in "$CONFIG_DIR/sddm/themes/"*; do
     sudo ln -sf "$file" "/usr/share/sddm/themes/$(basename $file)"
 done
 
-# sddm.conf
+# Archivo sddm.conf
 if [ -e "/etc/sddm.conf" ]; then
     echo "Respaldando /etc/sddm.conf"
     sudo mv "/etc/sddm.conf" "$BACKUP_DIR/"
@@ -52,4 +61,3 @@ echo "Creando symlink sddm.conf con sudo"
 sudo ln -sf "$CONFIG_DIR/sddm/sddm.conf" "/etc/sddm.conf"
 
 echo "Dotfiles instalados. Los archivos antiguos están en $BACKUP_DIR."
-
